e6b041af04f44712c1518ed57a17e77029f2cc02
fdoganis <fivos.doganis@gmail.com>
2026-03-03 11:13:59 +0100

    fix base URL in vite.config.js for proper asset loading, support ngrok

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import glsl from 'vite-plugin-glsl';
 import { injectIWER } from "@iwsdk/vite-plugin-iwer";
 
 export default defineConfig({
-    base: "/three_vite_xr_ts",
+    base: "./",
     clearScreen: false,
     optimizeDeps: {
         esbuildOptions: {
@@ -24,7 +24,7 @@ export default defineConfig({
     },
     server: {
         open: true,
-        allowedHosts: ['.trycloudflare.com']
+        allowedHosts: ['.trycloudflare.com', '.ngrok-free.app', '.ngrok-free.dev']
     },
     plugins: [
         viteStaticCopy({
